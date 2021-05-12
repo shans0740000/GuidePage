9b359eb52986fc90bbeadf62b4155134c839752d
shans0740000 <xulei1988zhongguo@163.com>
2021-05-12 13:35:16 +0800

    init

---
+++ GuidePage/GuidePageViewController.h
@@ -0,0 +1,16 @@
+//
+//  GuidePageViewController.h
+//  GuidePageDemo
+//
+//  Created by mac on 2021/5/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GuidePageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
